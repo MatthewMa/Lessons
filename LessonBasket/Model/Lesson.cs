95f802cabb7f3884c627bbbb68032105b1ccb07d
sgjp <sanguejuan@gmail.com>
2016-04-14 09:44:32 -0600

    Lesson Object

--- LessonBasket/Model/Lesson.cs
+++ LessonBasket/Model/Lesson.cs
@@ -1,9 +1,16 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace LessonBasket
 {
 	public class Lesson
 	{
+		public int id { get; set;}
+		public string description { get; set;}
+		public IList<string> screenList { get; set;}
+		public int screenCount { get; set;}
+		public string title { get; set;}
+
 		public Lesson ()
 		{
 		}
