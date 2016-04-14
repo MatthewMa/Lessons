8cfaf86c1b002130d1fc88ed1e64dad7928a099a
Juan Pablo Sanchez <sanguejuan@gmail.com>
2016-04-14 16:37:12 -0600

    Model and server communication

--- LessonBasket/Model/Image.cs
+++ LessonBasket/Model/Image.cs
@@ -4,6 +4,9 @@ namespace LessonBasket
 {
 	public class Image
 	{
+		public string url{ get; set;}
+		public string title{ get; set;}
+
 		public Image ()
 		{
 		}
--- LessonBasket/Model/Lesson.cs
+++ LessonBasket/Model/Lesson.cs
@@ -8,7 +8,7 @@ namespace LessonBasket
 
 		public int id { get; set;}
 		public string description { get; set;}
-		public IList<string> screenList { get; set;}
+		public IList<string> screenList { get; set;} //TODO this should not be string but the Screen object
 		public int screenCount { get; set;}
 		public string title { get; set;}
 
