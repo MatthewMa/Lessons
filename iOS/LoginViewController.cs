c72f0b81c025eab1c10a4093e2421735e62ae537
Nam Hoang <nam.xuan.hoang.work@gmail.com>
2016-04-14 00:06:57 -0600

    Add logo image.

--- iOS/LoginViewController.cs
+++ iOS/LoginViewController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using UIKit;
+using CoreGraphics;
 
 namespace LessonBasket.iOS
 {
@@ -16,9 +17,16 @@ namespace LessonBasket.iOS
             Title = "Login";
             View.BackgroundColor = UIColor.FromHSB(UIConstants.BackgroundColorHue, UIConstants.BackgroundColorSaturation, UIConstants.BackgroundColorBrightness);
 
+            var logo = new UIImageView();
+            View.AddSubview(logo);
+            logo.Image = UIImage.FromBundle("lesson_basket_logo.png");
+            logo.Layer.BorderColor = new CGColor(255, 255, 255);
+            logo.Layer.BorderWidth = 1f;
+            logo.Layer.CornerRadius = 25f;
+
             var username = new UITextField
             {
-                Placeholder = "Username",
+                Placeholder = "Email",
                 BorderStyle = UITextBorderStyle.RoundedRect,
             };
             View.AddSubview(username);
@@ -62,17 +70,24 @@ namespace LessonBasket.iOS
             signUpButton.Layer.CornerRadius = UIConstants.CornerRadius;
 
             #region UI Layout
-            var topPad = (float)NavigationController.NavigationBar.Frame.Size.Height + UIConstants.VerticalPad + 30f;
+//            var topPad = (float)NavigationController.NavigationBar.Frame.Size.Height + UIConstants.VerticalPad + 30f;
+            var topPad = View.Frame.GetCenterY() - 170f - 100f; // 170 is half the total heights of all controls
 
             View.ConstrainLayout(() =>
-                username.Frame.Top == View.Frame.Top + topPad &&
+                logo.Frame.Top == View.Frame.Top + topPad &&
+                logo.Frame.Left >= View.Frame.Left + UIConstants.HorizontalPad &&
+                logo.Frame.Right >= View.Frame.Right - UIConstants.HorizontalPad &&
+                logo.Frame.GetCenterX() == View.Frame.GetCenterX() &&
+                logo.Frame.Width == 500f &&
+
+                username.Frame.Top == logo.Frame.Bottom + 40f &&
                 username.Frame.Left >= View.Frame.Left + UIConstants.HorizontalPad &&
                 username.Frame.Right >= View.Frame.Right - UIConstants.HorizontalPad &&
                 username.Frame.Height == UIConstants.ControlsHeight &&
                 username.Frame.GetCenterX() == View.Frame.GetCenterX() &&
                 username.Frame.Width <= UIConstants.MaximumControlsWidth &&
 
-                password.Frame.Top == username.Frame.Bottom + 20f &&
+                password.Frame.Top == username.Frame.Bottom + 10f &&
                 password.Frame.Left >= View.Frame.Left + UIConstants.HorizontalPad &&
                 password.Frame.Right >= View.Frame.Right - UIConstants.HorizontalPad &&
                 password.Frame.Height == UIConstants.ControlsHeight &&
@@ -86,7 +101,7 @@ namespace LessonBasket.iOS
                 loginButton.Frame.GetCenterX() == View.Frame.GetCenterX() &&
                 loginButton.Frame.Width <= UIConstants.MaximumControlsWidth &&
 
-                signUpButton.Frame.Top == loginButton.Frame.Bottom + 20f &&
+                signUpButton.Frame.Top == loginButton.Frame.Bottom + 10f &&
                 signUpButton.Frame.Left >= View.Frame.Left + UIConstants.HorizontalPad &&
                 signUpButton.Frame.Right >= View.Frame.Right - UIConstants.HorizontalPad &&
                 signUpButton.Frame.Height == UIConstants.ControlsHeight &&
