cdfbc93c38aedb50f56644dbe41da29dbe4f014e
uurtsaikh201 <uuj476@mail.usask.ca>
2016-04-18 16:12:07 -0600

    complete screen object and brief lesson object

---
+++ Java/usis/src/to/LessonBr.java
@@ -0,0 +1,26 @@
+package to;
+
+public class LessonBr {
+	
+	Integer id;
+	String title,url;
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	
+}
